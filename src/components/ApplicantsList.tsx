a786e85216dcced9f5fdef10438779150a2fb45f
dianna <ic.diannajaneelizabeth.pacatang@cvsu.edu.ph>
2026-03-22 03:26:56 +0800

    ui

--- src/components/ApplicantsList.tsx
+++ src/components/ApplicantsList.tsx
@@ -444,8 +444,8 @@ export function ApplicantsList() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 p-6 lg:p-8">
-      <div className="max-w-7xl mx-auto space-y-6">
+    <div className="p-4 lg:p-6">
+      <div className="max-w-7xl mx-auto space-y-4">
         {/* Header */}
         <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4">
           <div>
